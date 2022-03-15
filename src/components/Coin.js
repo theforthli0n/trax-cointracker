859c6d10a5bc89d6f2c18e8321788b332ec443df
TheForthLion <the00110100lion@gmail.com>
2022-03-15 16:13:42 -0700

    added night and day mode

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -7,6 +7,7 @@ export default function Coin({
   priceChange,
   marketcap,
   marketcapRank,
+  ath,
 }) {
   return (
     <div className="Coin">
@@ -15,19 +16,25 @@ export default function Coin({
           <p className="mcap-rank">{marketcapRank}</p>
           <img className="coin-pic" alt="coin-pic" src={image} />
           <h1>{name}</h1>
-
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">${price}</p>
-          <p className="coin-volume">Total Volume: {volume.toLocaleString()}</p>
+          <p className="coin-ath">
+            ATH: ${ath > 999 ? ath.toLocaleString() : ath}
+          </p>
+          <p className="coin-price">
+            ${price > 999 ? price.toLocaleString() : price}
+          </p>
+          <p className="coin-volume">
+            Total Volume: ${volume.toLocaleString()}
+          </p>
           {priceChange < 0 ? (
             <p className="coin-percentage red">{priceChange.toFixed(2)}%</p>
           ) : (
             <p className="coin-percentage green">{priceChange.toFixed(2)}%</p>
           )}
           <p className="coin-marketcap">
-            Mkt Cap: ${marketcap.toLocaleString()}
+            Market Cap: ${marketcap.toLocaleString()}
           </p>
         </div>
       </div>
--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Coin from "./Coin";
+import { ReactComponent as Moon } from "../styles/images/moon.svg";
+import { ReactComponent as Sun } from "../styles/images/sun.svg";
 
 export default function Tracker() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [mode, setMode] = useState(false);
 
   //   grab api
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100loc&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
@@ -19,6 +22,7 @@ export default function Tracker() {
       .catch((error) => console.log(error));
   }, []);
 
+  //   filter through content with search bar
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -28,17 +32,52 @@ export default function Tracker() {
       coin.name.toLowerCase().includes(search.toLowerCase()) ||
       coin.symbol.toLowerCase().includes(search.toLowerCase())
   );
+
+  function Mode() {
+    const header = document.querySelectorAll("h1");
+    const para = document.querySelectorAll("p");
+    const coinRow = document.querySelectorAll(".coin-row");
+
+    if (mode === false) {
+      setMode(true);
+      document.querySelector("body").style.backgroundColor =
+        "rgb(228, 227, 222)";
+      //   document.querySelector("h1").style.color = "black";
+
+      for (let i = 0; i < header.length; i++) {
+        header[i].style.color = "rgb(44, 50, 59)";
+      }
+      for (let i = 0; i < para.length; i++) {
+        para[i].style.color = "rgb(44, 50, 59)";
+      }
+      for (let i = 0; i < coinRow.length; i++) {
+        coinRow[i].style.borderColor = "rgb(44, 50, 59)";
+      }
+    } else {
+      setMode(false);
+      document.querySelector("body").style.backgroundColor = "rgb(44, 50, 59)";
+      for (let i = 0; i < header.length; i++) {
+        header[i].style.color = "rgb(228, 227, 222)";
+      }
+      for (let i = 0; i < para.length; i++) {
+        para[i].style.color = "rgb(228, 227, 222)";
+      }
+      for (let i = 0; i < coinRow.length; i++) {
+        coinRow[i].style.borderColor = "rgb(228, 227, 222)";
+      }
+    }
+  }
   return (
     <div className="Tracker">
-      <div className="search-coin">
-        <h1 className="search-label">Search a currency</h1>
-        <input
-          type="text"
-          placeholder="Search"
-          className="coin-input"
-          onChange={handleChange}
-        />
-      </div>
+      {mode === true ? <Moon onClick={Mode} /> : <Sun onClick={Mode} />}
+      <h1 className="header">Trax Crypto Tracker</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        className="coin-input"
+        onChange={handleChange}
+      />
+
       {filteredCoins.map((coin) => {
         return (
           <Coin
@@ -51,6 +90,7 @@ export default function Tracker() {
             priceChange={coin.price_change_percentage_24h}
             marketcap={coin.market_cap}
             marketcapRank={coin.market_cap_rank}
+            ath={coin.ath}
           />
         );
       })}
